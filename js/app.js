83877364d251d74a94fccd36f514cd43f1470930
toddhollen <toddhollen@gmail.com>
2014-08-13 18:07:12 -0600

    Almost Final

--- js/app.js
+++ js/app.js
@@ -29,6 +29,19 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   });
 
+$(document).keydown(function(event) {
+	if ( event.which == 88)
+ 	$('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-cool').show();
+ });
+
+$(document).keyup(function(event) {
+	if ( event.which == 88)
+    $('.ryu-still').show();
+    $('.ryu-cool').hide();
+ });
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
